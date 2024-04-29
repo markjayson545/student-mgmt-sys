dae2bdd65ee6beda2b02ad06a28587e0890188db
markjayson545 <markjayson545@gmail.com>
2024-04-29 09:32:53 +0800

    master

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void getSelection(int sel){
+    
+}
+
+int main(){
+    int sel;
+    char selC;
+
+    cout << "Student Management System\n";
+    cout << "Menu: 1. Manage Student Records\n";
+    cout << "      2. Student Registration and Enrollment\n";
+    cout << "      3. Generate Reports\n";
+    cout << "Selection: ";
+    cin >> sel;
+    getSelection(sel);
+return 0;
+}
