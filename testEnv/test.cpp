cbc167afca20b2098231f68b387d5ea6e765ba52
markjayson545 <markjayson545@gmail.com>
2024-04-29 12:47:14 +0800

    learning sqlite

--- testEnv/test.cpp
+++ testEnv/test.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){
+        sqlite3 *db; // Database handle
+        int rc;
 
+        rc = sqlite3_open("mydatabase.db", &db);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "Error opening database: %s\n", sqlite3_errmsg(db));
+            sqlite3_close(db);
+            return 1; // Or handle the error appropriately
+        }
     return 0;
 }
