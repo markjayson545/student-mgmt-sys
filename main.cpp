7de1456e19582db630df7c309c51a10c782ea905
markjayson545 <markjayson545@gmail.com>
2024-04-29 12:19:14 +0800

    master

--- main.cpp
+++ main.cpp
@@ -1,20 +1,69 @@
 #include <iostream>
 using namespace std;
 
-void getSelection(int sel){
+class dBaseAccess{
+    private:
+
+    public:
     
-}
+};
+
+class processing: public dBaseAccess{
+    private:
+
+    void mgmStdRecords(){
+        
+    }
+    void stdRegNenroll(){
+
+    }
+    void genReports(){
+
+    }
+
+    public:
+
+};
+
+class getInputs: public processing{
+    private:
+        void getSel(int sel){
+            switch (sel)
+            {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+            cout << "Invalid Selection";
+                break;
+            }
+        }
+    public:
+        void runSel(int sel){
+            getSel(sel);
+        }
+};
+
+
 
 int main(){
+    getInputs inputs;
     int sel;
     char selC;
 
     cout << "Student Management System\n";
-    cout << "Menu: 1. Manage Student Records\n";
-    cout << "      2. Student Registration and Enrollment\n";
-    cout << "      3. Generate Reports\n";
+    cout << "-----------Menu-----------\n";
+    cout << " 1. Manage Student Records\n";
+    cout << " 2. Student Registration and Enrollment\n";
+    cout << " 3. Generate Reports\n";
     cout << "Selection: ";
     cin >> sel;
-    getSelection(sel);
+    inputs.runSel(sel);
 return 0;
 }
